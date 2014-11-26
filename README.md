50a70fb8ddb9bea00db2c66de679f7d8812f49ec
Fasih <faskiri@instartlogic.com>
2014-11-26 00:03:47 -0800

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ google-drive-extract-images
 ===========================
 
 Extracts images from google drive using Google Drive APIs
+
+The code is super simple and does not intend to handle any corner cases. It was really a single use thing that I have saved to github. Feel free to use as a sample tool to see how google api is used. Among other missing things, I have been lazy enough to not have implemented a proper CLI to ask for cliend id etc. To use, you have to open the file and modify the following:
+
+CLIENT_ID = 'CLIENT_ID'
+CLIENT_SECRET = 'CLIENT_SECRET'
+folder_id = 'folder-id'
+
+Google API enabling
+-------------------
+From https://developers.google.com/drive/web/quickstart/quickstart-python#step_1_enable_the_drive_api
+1. Go to the Google Developers Console.
+2. Select a project, or create a new one.
+3. In the sidebar on the left, expand APIs & auth. Next, click APIs. In the list of APIs, make sure the status is ON for the Drive API.
+4. In the sidebar on the left, select Credentials.
+
---
+++ main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+import argparse
+import httplib2
+import os
+import pprint
+import StringIO
+import zipfile
+
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from apiclient import errors
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
+from oauth2client import tools
+
+# Copy your credentials from the console
+CLIENT_ID = 'CLIENT_ID'
+CLIENT_SECRET = 'CLIENT_SECRET'
+folder_id = 'folder-id'
+
+def get_credentials(path):
+  storage = Storage(path)
+  if os.path.exists(path):
+    return storage.get()
+
+  # Check https://developers.google.com/drive/scopes for all available scopes
+  OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive.readonly'
+
+  # Redirect URI for installed apps
+  REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+  # Run through the OAuth flow and retrieve credentials
+  flow = OAuth2WebServerFlow(
+      CLIENT_ID,
+      CLIENT_SECRET,
+      OAUTH_SCOPE,
+      redirect_uri=REDIRECT_URI)
+
+  parser = argparse.ArgumentParser(parents=[tools.argparser])
+  flags = parser.parse_args()
+  credentials = tools.run_flow(flow, storage, flags)
+  storage.put(credentials)
+  return credentials
+
+credentials = get_credentials('credentials.db')
+
+# Create an httplib2.Http object and authorize it with our credentials
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+drive_service = build('drive', 'v2', http=http)
+
+page_token = None
+while True:
+  try:
+    param = {}
+    if page_token:
+      param['pageToken'] = page_token
+    children = drive_service.children().list(
+        folderId=folder_id, **param).execute()
+
+    for child in children.get('items', []):
+      file = drive_service.files().get(fileId=child['id']).execute()
+      print 'Downloading title: %s' % file['title']
+      links = file.get('exportLinks')
+      if links:
+        openxml = links.get('application/vnd.openxmlformats-officedocument.presentationml.presentation')
+        if openxml:
+          resp, content = drive_service._http.request(openxml)
+          if resp.status == 200:
+            with zipfile.ZipFile(StringIO.StringIO(content)) as z:
+              for name in z.namelist():
+                if 'media' in name:
+                  if not os.path.exists('media'): os.mkdir('media')
+                  with z.open(name) as zi, open('media/%s-%s' % (
+                      file['title'].replace(' ', '_'),
+                      name.replace('/', '-')), 'w') as media:
+                    media.write(zi.read())
+
+    page_token = children.get('nextPageToken')
+    if not page_token:
+      break
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+    break
